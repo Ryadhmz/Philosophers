4273205eba277b6cba8e5c2987135df3ba30043f
Ryadhmz <109631638+Ryadhmz@users.noreply.github.com>
2023-03-18 07:03:39 +0100

    first commit

---
+++ philo/philosophers.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhamza <rhamza@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/18 06:45:48 by rhamza            #+#    #+#             */
+/*   Updated: 2023/03/18 07:03:00 by rhamza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
