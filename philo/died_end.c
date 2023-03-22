28db6562e909736543514df035055bd13d337473
Ryadhmz <109631638+Ryadhmz@users.noreply.github.com>
2023-03-22 14:38:03 +0100

    add header

--- philo/died_end.c
+++ philo/died_end.c
@@ -1,39 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.c                                            :+:      :+:    :+:   */
+/*   died_end.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rhamza <rhamza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/22 14:37:53 by rhamza            #+#    #+#             */
-/*   Updated: 2023/03/22 14:37:55 by rhamza           ###   ########.fr       */
+/*   Created: 2023/03/22 14:37:22 by rhamza            #+#    #+#             */
+/*   Updated: 2023/03/22 14:37:22 by rhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philosophers.h"
-
-int	ft_atoi(const char *str)
-{
-	int		i;
-	int		result;
-
-	i = 0;
-	result = 0;
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		result = result * 10 + str[i] - '0';
-		i++;
-	}
-	return (result);
-}
-
-int ft_strlen(const char *str)
-{
-    int i;
-
-    i = 0;
-    while(str[i])
-        i++;
-
-    return(i);
-}
