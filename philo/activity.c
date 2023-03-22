b398167a726e1b8960fe6dd28966d649d7960b91
ryadhmz <ryadhamzaa@outlook.fr>
2023-03-22 02:02:03 +0100

    activity fait jusqua is sleeping

--- philo/activity.c
+++ philo/activity.c
@@ -10,5 +10,19 @@ void activity(t_phil *ph)
     pthread_mutex_lock(&(ph->f_r));
     pthread_mutex_lock(&(ph->mutex_write)); // donner a tout les threads le MEME mutex write
     print_activity(ph->id, "has taken a fork");
-    pthread_mutex_destroy()
+    pthread_mutex_destroy(&(ph->mutex_write));
+    pthread_mutex_lock(&(ph->f_l));
+    pthread_mutex_lock(&(ph->mutex_write));
+    print_activity(ph->id, "has taken a fork");
+    pthread_mutex_destroy(&(ph->mutex_write));
+    pthread_mutex_lock(&(ph->mutex_write));
+    print_activity(ph->id, "is eating");
+    pthread_mutex_destroy(&(ph->mutex_write));
+    better_sleep(ph->arg.time_to_eat);
+    pthread_mutex_destroy(&(ph->f_r));
+    pthread_mutex_destroy(&(ph->f_l));
+    pthread_mutex_lock(&(ph->mutex_write));
+    print_activity(ph->id, "is sleeping");
+    pthread_mutex_destroy(&(ph->mutex_write));
+    
 }
