525cd368a052cd8b226d6863ea5c6e2ebf190b7d
Ryadhmz <109631638+Ryadhmz@users.noreply.github.com>
2023-03-29 23:17:46 +0200

    go fonction solo a part

--- philo/activity.c
+++ philo/activity.c
@@ -6,7 +6,7 @@
 /*   By: rhamza <rhamza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 14:37:44 by rhamza            #+#    #+#             */
-/*   Updated: 2023/03/29 14:59:05 by rhamza           ###   ########.fr       */
+/*   Updated: 2023/03/29 23:17:21 by rhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,30 +17,42 @@ void print_activity(int fd, char *str)
     printf("%lld %u %s\n", ft_actual_time(), fd, str);
 }
 
+int solo_philo(t_phil *ph)
+{
+    better_sleep(ph->arg->time_to_eat);
+    pthread_mutex_unlock(&(ph->f_r));
+    better_sleep(ph->arg->time_to_eat);
+    return(-1);
+}
+
 int activity(t_phil *ph)
 {
-    ph->begin_activity = ft_actual_time();
+    ph->begin_activity = ft_actual_time(); 
     pthread_mutex_lock(&(ph->f_r));
     pthread_mutex_lock((ph->ptr_mutex_write)); // donner a tout les threads le MEME mutex write
     print_activity(ph->id, "has taken a fork");
-    pthread_mutex_destroy((ph->ptr_mutex_write));
+    pthread_mutex_unlock((ph->ptr_mutex_write));
+    if(ph->arg->nb_phil == 1)
+        return(solo_philo(ph));
+    printf("unlock 1\n");
+    if(ph->f_l != NULL)
     pthread_mutex_lock((ph->f_l));
     pthread_mutex_lock((ph->ptr_mutex_write));
     print_activity(ph->id, "has taken a fork");
-    pthread_mutex_destroy((ph->ptr_mutex_write));
+    pthread_mutex_unlock((ph->ptr_mutex_write));
     pthread_mutex_lock((ph->ptr_mutex_write));
     print_activity(ph->id, "is eating");
-    pthread_mutex_destroy((ph->ptr_mutex_write));
+    pthread_mutex_unlock((ph->ptr_mutex_write));
     better_sleep(ph->arg->time_to_eat);
-    pthread_mutex_destroy(&(ph->f_r));
-    pthread_mutex_destroy((ph->f_l));
+    pthread_mutex_unlock(&(ph->f_r));
+    pthread_mutex_unlock((ph->f_l));
     pthread_mutex_lock((ph->ptr_mutex_write));
     print_activity(ph->id, "is sleeping");
-    pthread_mutex_destroy((ph->ptr_mutex_write));
+    pthread_mutex_unlock((ph->ptr_mutex_write));
     better_sleep(ph->arg->time_to_sleep);
     pthread_mutex_lock((ph->ptr_mutex_write));
     print_activity(ph->id, "is thinking");
-    pthread_mutex_destroy((ph->ptr_mutex_write));
+    pthread_mutex_unlock((ph->ptr_mutex_write));
     if(if_died(ph) == -1)
         return (-1);
     if(ph->nb_eat >= ph->arg->each_phil_m_eat)
